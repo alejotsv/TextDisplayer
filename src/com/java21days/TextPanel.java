83c348d94f395b1c7b5f08577982f9accaa376f3
Alejandro Salas <asalas.wd@outlook.com>
2022-11-23 15:19:13 -0500

    Add text with different font

--- src/com/java21days/TextPanel.java
+++ src/com/java21days/TextPanel.java
@@ -14,6 +14,9 @@ public class TextPanel extends JPanel {
     @Override
     public void paintComponent(Graphics comp){
         Graphics2D comp2D = (Graphics2D) comp;
-        comp2D.drawString(text, 10, 60);
+        comp2D.drawString(text, 60, 140);
+        Font font = new Font("Comic Sans", Font.BOLD + Font.ITALIC, 12);
+        comp2D.setFont(font);
+        comp2D.drawString("With a different font", 60, 40);
     }
 }
