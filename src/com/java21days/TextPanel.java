8012185875703b08615a0e2881c5315eee39e1f5
Alejandro Salas <asalas.wd@outlook.com>
2022-11-23 15:09:19 -0500

    Complete TextPanel method to draw String

--- src/com/java21days/TextPanel.java
+++ src/com/java21days/TextPanel.java
@@ -1,6 +1,19 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TextPanel extends JPanel {
+    String text;
+
+    TextPanel(String text){
+        super();
+        this.text = text;
+    }
+
+    @Override
+    public void paintComponent(Graphics comp){
+        Graphics2D comp2D = (Graphics2D) comp;
+        comp2D.drawString(text, 10, 10);
+    }
 }
